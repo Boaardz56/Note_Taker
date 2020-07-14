5316009dcc9d1d0f0732ae95cd916d7a413f6ea2
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-14 17:18:23 -0400

    added to app.js and created function in apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,16 @@
-var path = require("path");
+var database = require("../db/db");
 
 module.exports = function(app) {
 //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
+app.get("/api/notes", function(req, res) {
+    res.json(db);
+})
 
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
+app.post("/api/tables", function(req, res) {
+    db.push(req.body);
+    return;
+})
 
 //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
 //This means you'll need to find a way to give each note a unique id when it's saved. 
